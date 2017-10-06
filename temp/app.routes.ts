0ad9f8f3c70a7c48506fa3219587ad34942d96cd
kostamas <kosta.masliansky@gmail.com>
2017-10-06 21:03:14 +0300

    Delete app.routes.ts

--- temp/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Routes} from '@angular/router';
-import {AComponent} from './componentA/a.component';
-import {BComponent} from './componentB/b.component';
-
-export const appRoutes: Routes = [
-  {path: '', component: AComponent}
-];
