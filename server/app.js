8f544e2d22ea7e4886b82b0dd1317a499b44e76e
kostamas <kosta.masliansky@gmail.com>
2017-10-09 16:20:30 +0300

    add new html files

--- server/app.js
+++ server/app.js
@@ -5,20 +5,23 @@ const renderModuleFactory = require('@angular/platform-server').renderModuleFact
 const zone = require('zone.js');
 const fs = require('fs');
 
-const document = fs.readFileSync(__dirname + '/../src/index.html', 'utf8');
 const AppServerModuleNgFactory = require(__dirname + '/dist-server/main.bundle').AppServerModuleNgFactory;
 
-function serverRouteHandler(req, res) {
-  const url = req.path;
-  renderModuleFactory(AppServerModuleNgFactory, {document, url}).then(html=>{
-    res.set( 'Cache-Control', 'public', 'max-age=600', 's-maxage=1200');
-    res.send(html)
-  })
+function serverRouteHandler(htmlFileName) {
+  const document = fs.readFileSync(__dirname + `/${htmlFileName}`, 'utf8');
+
+  return function (req, res) {
+    const url = req.path;
+    renderModuleFactory(AppServerModuleNgFactory, {document, url}).then(html => {
+      res.set('Cache-Control', 'public', 'max-age=600', 's-maxage=1200');
+      res.send(html)
+    })
+  }
 }
 
-app.get('',serverRouteHandler);
-app.get('/a',serverRouteHandler);
-app.get('/b',serverRouteHandler);
+app.get('', serverRouteHandler('index.html'));
+app.get('/a', serverRouteHandler('a.html'));
+app.get('/b', serverRouteHandler('b.html'));
 
 app.listen(3000, function () {
   console.log('listening on port 3000!')
