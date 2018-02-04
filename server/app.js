b89e913877638e10b7b890669da4ea5e36c7e62f
kosta mas <kosta.masliansky@gmail.com>
2018-02-05 00:20:15 +0200

    save

--- server/app.js
+++ server/app.js
@@ -1,28 +1,30 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
-const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
-const zone = require('zone.js');
-const fs = require('fs');
 
-const AppServerModuleNgFactory = require(__dirname + '/dist-server/main.bundle').AppServerModuleNgFactory;
-
-function serverRouteHandler(htmlFileName) {
-  const document = fs.readFileSync(__dirname + `/${htmlFileName}`, 'utf8');
-
-  return function (req, res) {
-    const url = req.path;
-    renderModuleFactory(AppServerModuleNgFactory, {document, url}).then(html => {
-      res.set('Cache-Control', 'public', 'max-age=600', 's-maxage=1200');
-      res.send(html)
-    })
-  }
-}
-
-app.get('', serverRouteHandler('index.html'));
-app.get('/a', serverRouteHandler('a.html'));
-app.get('/b', serverRouteHandler('b.html'));
-
-app.listen(3000, function () {
-  console.log('listening on port 3000!')
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+app.get('/', (req, res) => {
+
+  res.send('Hello World!')
+});
+app.post('/pdf', function (req, res, next) {
+
+  var filePath = './server/p.pdf';
+
+  var data = fs.readFileSync(filePath);
+
+  var headers = {
+    'Content-type': 'application/octet-stream',
+    'Content-Disposition': 'attachment; filename=sample'
+  };
+  res.writeHead(200, headers);
+  res.end(data);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
